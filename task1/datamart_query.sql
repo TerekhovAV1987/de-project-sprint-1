abfd03b592647e9f31fd46c2ce04012d0919d316
Terekhov Alexander <swimalext@gmail.com>
2023-05-24 11:12:25 +0300

    Sprint_1, обновлена таблица analysis.dm_rfm_segments

--- task1/datamart_query.sql
+++ task1/datamart_query.sql
@@ -1,3 +1,5 @@
+truncate table analysis.dm_rfm_segments;
+
 insert into analysis.dm_rfm_segments
 select t1.user_id,
        t1.recency,
@@ -5,15 +7,15 @@ select t1.user_id,
        t3.monetary_value
 from analysis.tmp_rfm_recency t1
 join analysis.tmp_rfm_frequency t2 on (t1.user_id = t2.user_id)
-join analysis.tmp_rfm_monetary_value on t3 (t1.user_id = t3.user_id);
+join analysis.tmp_rfm_monetary_value t3 on (t1.user_id = t3.user_id);
 
-0	5	3	4
-1	2	3	3
-2	4	3	5
-3	4	3	3
-4	2	3	3
-5	2	5	5
-6	5	3	5
-7	2	2	2
-8	5	1	3
-9	5	2	2
+224	5	1	1
+467	5	1	1
+837	5	1	1
+514	5	1	1
+211	5	1	1
+821	5	1	1
+889	5	1	1
+930	5	1	1
+784	5	1	1
+977	5	1	1
