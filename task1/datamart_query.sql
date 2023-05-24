f54487b208e9ecd5920adc45e0f7428ab7bc914d
Terekhov Alexander <swimalext@gmail.com>
2023-05-24 11:19:07 +0300

    Sprint_1, обновлена таблица analysis.dm_rfm_segments

--- task1/datamart_query.sql
+++ task1/datamart_query.sql
@@ -9,13 +9,13 @@ from analysis.tmp_rfm_recency t1
 join analysis.tmp_rfm_frequency t2 on (t1.user_id = t2.user_id)
 join analysis.tmp_rfm_monetary_value t3 on (t1.user_id = t3.user_id);
 
-224	5	1	1
-467	5	1	1
-837	5	1	1
-514	5	1	1
-211	5	1	1
-821	5	1	1
-889	5	1	1
-930	5	1	1
-784	5	1	1
-977	5	1	1
+0	1	3	4
+1	4	3	3
+2	2	3	5
+3	2	3	3
+4	4	3	3
+5	5	5	5
+6	1	3	5
+7	4	2	2
+8	1	1	3
+9	1	3	2
